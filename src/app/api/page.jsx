84eacbff6eb7f14f4663ef988978a63741ca976d
Albertojs89 <jimenezsanchezalberto@fpllefia.com>
2025-06-03 18:22:02 +0200

    congifuracion OK, header y footer

---
+++ src/app/api/page.jsx
@@ -0,0 +1,8 @@
+"use client";
+
+
+export default function ApiPage() {
+  return (
+    <h1>Vista peliculas</h1>
+  );
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import ApiPage from "./api/page";
+
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center h-full text-white">
+      
+      <ApiPage />
+    </div>
+  );
+}
